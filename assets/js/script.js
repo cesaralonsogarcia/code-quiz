51acae4fdfb119d3c5596223ee2d148b4560b9fd
Cesar A. Garcia <121130408+cesaralonsogarcia@users.noreply.github.com>
2023-03-15 17:19:56 -0500

    Started writing JS script

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Variables to access the HTML elements
+var container = document.querySelector('#container');
+var questionContainer = document.querySelector('#questionContainer');
+var optionContainer = document.querySelector('#optionContainer');
+var gameDescription = document.querySelector('#gameDescription');
+var answerList = document.querySelector('#answerList');
+var startButton = document.querySelector('#startButton');
+
+// Array with quiz questions
+var questionsArray = [];
+var questionOne = {};
